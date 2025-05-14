1d26068238b1a1a5373f3e94255a3eca3f89f8d1
KrishO9 <krish27n@gmail.com>
2025-05-14 18:37:02 +0530

    working testcases

--- pages/tests.py
+++ pages/tests.py
@@ -8,25 +8,25 @@ class PageViewTests(SimpleTestCase):
         self.client = Client()
 
     def test_home_page_view_status_code(self):
-        response = self.client.get(reverse('home_page')) # Use reverse for URL names
+        response = self.client.get(reverse('home')) # Use reverse for URL names
         self.assertEqual(response.status_code, 200)
 
     def test_home_page_view_content(self):
-        response = self.client.get(reverse('home_page'))
+        response = self.client.get(reverse('home'))
         self.assertContains(response, "<h1> Hello There </h1>", html=True)
         self.assertContains(response, "<p> Just the home page </p>", html=True)
 
     def test_greet_view_status_code(self):
-        response = self.client.get(reverse('greet_user', args=['Alice']))
+        response = self.client.get(reverse('greet', args=['Alice']))
         self.assertEqual(response.status_code, 200)
 
     def test_greet_view_content_with_name(self):
         name = "Bob"
-        response = self.client.get(reverse('greet_user', args=[name]))
+        response = self.client.get(reverse('greet', args=[name]))
         self.assertContains(response, f"<h1>Hello, {name}!</h1>", html=True)
         self.assertContains(response, "<p>Nice to meet you.</p>", html=True)
 
     def test_greet_view_content_with_different_name(self):
         name = "Charlie"
-        response = self.client.get(reverse('greet_user', args=[name]))
+        response = self.client.get(reverse('greet', args=[name]))
         self.assertContains(response, f"<h1>Hello, {name}!</h1>", html=True)
